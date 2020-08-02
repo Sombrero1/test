692dd424357b2ed0daee1873ca54abd3233ec544
root <root@DESKTOP-676M087.localdomain>
2020-08-02 19:05:13 +0300

    test2

--- py.py
+++ py.py
@@ -1,1 +1,27 @@
-print("ААА")
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+       r=urllib.request.urlopen(self.site)
+       html=r.read()
+       parser="html.parser"
+       s=BeautifulSoup(html, parser)
+       for tag in s.find_all("a"):
+           url = tag.get("href")
+           if url is None:
+               continue
+           if "http" in url:
+               print("\n" + url)
+        
+            
+
+       
+       
+news = "https://pythonru.com/baza-znanij/ustanovka-pip-dlja-python-i-bazovye-komandy"
+Scraper(news).scrape()
+
